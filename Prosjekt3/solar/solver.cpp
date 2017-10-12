878e90d9f3291f3a5a75044c696c7087bf0e084c
hannahberg <hannah.brg@gmail.com>
2017-10-12 14:41:21 +0200

    maybe

--- Prosjekt3/solar/solver.cpp
+++ Prosjekt3/solar/solver.cpp
@@ -21,15 +21,14 @@ solver::solver(double radi){
     radius = radi;
     mass_tot = 0;
     G = 4*pi*pi;
-    kinetic_tot = 0;
-    potential_tot = 0;
+
 }
 
-void solver::G_constant(){
-    G = (4*pi*pi/32)*radius*radius*radius/mass_tot;
+void solver::G_constant(){ //double mass4pi){
+    G = 4*pi*pi// mass4pi; //radius*radius*radius;
 }
 
-void solver::print_trajectory(std:ofstram &output, string filename, int N, int dimensions, double time){ // write mass, trajectory and velocity to file 'output'
+/*void solver::print_trajectory(std:ofstram &output, string filename, int N, int dimensions, double time){ // write mass, trajectory and velocity to file 'output'
     if (dimensions > 3 || dimensions <= 0) dimensions = 3;
 
     else{
@@ -42,21 +41,40 @@ void solver::print_trajectory(std:ofstram &output, string filename, int N, int d
         }
     }
 
-
-void velVerlet(){
+*/
+void solver::velVerlet(int dimensions, double h, int Nplanets){
 
 
     // define time step size h = t_final - t_initial / integration points
-    double h = final_time/((double) N);
-    double time = 0.0;
+    //double h = final_time/((double) N);
+    //double time = 0.0;
+
+    // make arrays for position,
+
+    vec position = zeros<vec>(dimensions);
+    vec velocity = zeros<vec>(dimensions);
+    vec acceleration = zeros<vec>(dimensions);// need to initialize the start
+    vec newacceleration = zeros<vec>(dimensions);
+    // planets = [Sun, Earth,Mars]
+    for(int j=0;j<Nplanets;j++){
+        planets(j).position += h*planets(j).velocity + 0.5*h*h*planets(j).acceleration;
+        planets(j).newacceleration = zeros<vec>(dimensions); //somefunctionthatcalculatesacceleration();
+        planets(j).velocity += 0.5*h*(planets(j).acceleration+planets(j).newacceleration);
+        planets(j).acceleraton = planets(j).newacceleration;
+
+    }
+    for(int i = 0; i<N-1; i++){
+
+
+
+
+        // call on print function
+        // print to file?
+
+    }
+
 
 
     // set up arrays for positions
-
-
-
-
-
-}
 
 }
--- Prosjekt3/solar/solver.h
+++ Prosjekt3/solar/solver.h
@@ -1,11 +1,36 @@
 #ifndef SOLVER_H
 #define SOLVER_H
-
+#include "planet.h"
+#include <vector>
+#include <fstream>
+using std::vector;
 
 class solver
 {
 public:
+    friend class planet;
+
+    // what kind of properties are there in the solver
+    double radius, mass_tot, G;
+    int Nplanets; // number of planets in the system we look at
+
+
+    // the energy in the system
+    double kinetic_tot, potential_tot;
+
+
+    // initializers
     solver();
+    solver(double radi);
+
+    // functions in the class solver
+    //void add(planet newplanet);
+    void G_constant();
+    //void print_trajectory();//std::ofstream &output, int dimensions, double time, int number);
+    void velVerlet(int dimensions, double h, int Nplanets);
+
+
+
 };
 
 #endif // SOLVER_H
